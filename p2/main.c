a20313cf4ab75716aca61ffff0c6a822ab79c6cd
2025050061 <chaeekimpd0215@gmail.com>
2026-04-15 16:18:26 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -5,18 +5,14 @@ int main(void)
     int N, numcnt=0, smallcnt=0;
     int maxnum=0, maxsmall=0;
     char ch;
-    bool is_num = false, is_small = false;
     bool is_in_num = false, is_in_small = false;
     
-    scanf("%d\n", &N);
+    scanf("%d", &N);
     
     for(int i=0; i<N; i++) {
         scanf("%c", &ch);
         
-        is_num = (ch>='0'&&ch<='9');
-        is_small = (ch>='a'&&ch<='z');
-        
-        if (is_num) {
+        if (ch>='0'&&ch<='9') {
             if (!is_in_num) {
                 is_in_num = true;
                 is_in_small = false;
@@ -27,7 +23,7 @@ int main(void)
             if (numcnt>maxnum) maxnum = numcnt;
         }
         
-        if (is_small) {
+        if (ch>='a'&&ch<='z') {
             if (!is_in_small) {
                 is_in_small = true;
                 is_in_num = false;
